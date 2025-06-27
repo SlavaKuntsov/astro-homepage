6a81fe4ad8bb04f6d8793584d6c0ba6f8e15cf7d
slava <75099655+SlavaKuntsov@users.noreply.github.com>
2025-06-27 17:08:44 +0300

    feat: default index page with toggle theme

---
+++ src/src/components/ThemeToggle.jsx
@@ -0,0 +1,71 @@
+import "../styles/global.css";
+import { useEffect, useState } from 'react'
+import { IoSunny } from "react-icons/io5";
+import { IoMoon } from "react-icons/io5";
+
+const themes = ['light', 'dark']
+
+export default function ThemeToggle() {
+    const [isMounted, setIsMounted] = useState(false)
+    const [showButton, setShowButton] = useState(false)
+    const [theme, setTheme] = useState(() => {
+        if (import.meta.env.SSR) {
+            return undefined
+        }
+        if (
+            typeof localStorage !== 'undefined' &&
+            localStorage.getItem('theme')
+        ) {
+            return localStorage.getItem('theme')
+        }
+        if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            return 'dark'
+        }
+        return 'light'
+    })
+
+    const toggleTheme = () => {
+        const t = theme === 'light' ? 'dark' : 'light'
+        localStorage.setItem('theme', t)
+        setTheme(t)
+    }
+
+    useEffect(() => {
+        const root = document.documentElement
+        if (theme === 'light') {
+            root.classList.remove('dark')
+        } else {
+            root.classList.add('dark')
+        }
+    }, [theme])
+
+    useEffect(() => {
+        setIsMounted(true)
+        setTimeout(() => setShowButton(true), 10)
+    }, [])
+
+    return isMounted ? (
+        <div className='inline-flex items-center p-[1px] rounded-3xl bg-orange-300 dark:bg-zinc-600'>
+            {themes.map(t => {
+                const checked = t === theme
+                return (
+                    <button
+                        className={`${checked ? 'bg-white text-zinc-900' : ''} cursor-pointer rounded-3xl p-2 flex items-center justify-center fade-in-btn${showButton ? ' fade-in-btn--visible' : ''}`}
+                        onClick={toggleTheme}
+                        aria-label={`Switch to ${t} mode`}
+                        key={t}
+                        style={{ position: 'relative' }}
+                    >
+                        {t === 'light' ?
+                            <IoSunny />
+                            :
+                            <IoMoon />
+                        }
+                    </button>
+                )
+            })}
+        </div>
+    ) : (
+        <div className='h-[26px]' />
+    )
+}
---
+++ src/src/consts.ts
@@ -0,0 +1,6 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Slava Kuntsov - Homepage';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const HOMEPAGE_URL = 'http://slavakuntsov-homepage'
