1563d28bac63ace51362b890b1b04d2752568fac
slava <75099655+SlavaKuntsov@users.noreply.github.com>
2025-06-28 00:43:08 +0300

    refactor: folders

---
+++ astro.config.mjs
@@ -0,0 +1,34 @@
+import { defineConfig, fontProviders } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
+import { fileURLToPath } from 'node:url';
+import { URL } from 'node:url';
+import { HOMEPAGE_URL } from './src/consts';
+
+export default defineConfig({
+  site: HOMEPAGE_URL,
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  },
+  integrations: [react(), mdx()],
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: 'Comfortaa',
+        weights: [300, 400, 500, 600, 700],
+        cssVariable: '--font-roboto',
+      },
+    ],
+  },
+  i18n: {
+    locales: ['ru', 'en'],
+    defaultLocale: 'en',
+  },
+});
