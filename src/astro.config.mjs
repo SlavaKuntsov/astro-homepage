389da2f16cffc4cef0664796c78943e0f28428f0
slava <75099655+SlavaKuntsov@users.noreply.github.com>
2025-06-28 00:20:58 +0300

    feat: dropdown menu on mobile

--- src/astro.config.mjs
+++ src/astro.config.mjs
@@ -1,30 +1,34 @@
-// @ts-check
 import { defineConfig, fontProviders } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
-
 import mdx from '@astrojs/mdx';
+import { fileURLToPath } from 'node:url';
+import { URL } from 'node:url';
 import { HOMEPAGE_URL } from './src/consts';
 
-// https://astro.build/config
 export default defineConfig({
   site: HOMEPAGE_URL,
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
   },
   integrations: [react(), mdx()],
   experimental: {
-    fonts: [{
-      provider: fontProviders.google(),
-      name: "Comfortaa",
-      weights: [300, 400, 500, 600, 700],
-      cssVariable: "--font-roboto"
-    }]
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: 'Comfortaa',
+        weights: [300, 400, 500, 600, 700],
+        cssVariable: '--font-roboto',
+      },
+    ],
   },
   i18n: {
-    locales: ["ru", "en"],
-    defaultLocale: "en"
-  }
+    locales: ['ru', 'en'],
+    defaultLocale: 'en',
+  },
 });
