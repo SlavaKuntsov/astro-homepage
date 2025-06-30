db0004f6ed8a817ceee37d2af3aa0152f3b80139
slava <75099655+SlavaKuntsov@users.noreply.github.com>
2025-06-30 22:59:04 +0300

    feat: carousel with arts

---
+++ src/src/components/custom/carousel.tsx
@@ -0,0 +1,42 @@
+import '../../styles/global.css';
+import {
+	Carousel as CarouselComponent,
+	CarouselContent,
+	CarouselItem,
+	CarouselNext,
+	CarouselPrevious,
+} from '@/components/ui/carousel';
+import Autoplay from 'embla-carousel-autoplay';
+
+export function Carousel() {
+	return (
+		<CarouselComponent
+			className='w-full max-w-xs'
+			opts={{
+				loop: true,
+			}}
+			plugins={[
+				Autoplay({
+					delay: 4000,
+				}),
+			]}
+		>
+			<CarouselContent>
+				{Array.from({ length: 3 }).map((_, index) => (
+					<CarouselItem key={index}>
+						<div
+							className='min-h-[250px] bg-transparent flex items-center justify-center w-full bg-center bg-cover shadow-none'
+							style={{
+								backgroundImage: `url(/images/arts2/art${index + 1}.png)`,
+							}}
+							aria-label={`Art ${index + 1}`}
+							role='img'
+						/>
+					</CarouselItem>
+				))}
+			</CarouselContent>
+			<CarouselPrevious className='bg-white/75 hover:bg-orange-300 transition-colors duration-500' />
+			<CarouselNext className='bg-white/75 hover:bg-orange-300 transition-colors duration-500' />
+		</CarouselComponent>
+	);
+}
