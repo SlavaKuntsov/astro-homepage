deb753d5ae029c25b171128e8d9847823247b3bf
slava <75099655+SlavaKuntsov@users.noreply.github.com>
2025-07-01 22:44:04 +0300

    feat: add arts

--- src/src/components/custom/carousel.tsx
+++ src/src/components/custom/carousel.tsx
@@ -6,9 +6,11 @@ import {
 	CarouselNext,
 	CarouselPrevious,
 } from '@/components/ui/carousel';
+import { useIsMobile } from '@/hooks/use-mobile';
 import Autoplay from 'embla-carousel-autoplay';
 
 export function Carousel() {
+	const isMobile = useIsMobile();
 	return (
 		<CarouselComponent
 			className='w-full max-w-xs'
@@ -22,7 +24,7 @@ export function Carousel() {
 			]}
 		>
 			<CarouselContent>
-				{Array.from({ length: 3 }).map((_, index) => (
+				{Array.from({ length: 6 }).map((_, index) => (
 					<CarouselItem key={index}>
 						<div
 							className='min-h-[250px] bg-transparent flex items-center justify-center w-full bg-center bg-cover shadow-none'
@@ -35,8 +37,12 @@ export function Carousel() {
 					</CarouselItem>
 				))}
 			</CarouselContent>
-			<CarouselPrevious className='bg-white/75 hover:bg-orange-300 transition-colors duration-500' />
-			<CarouselNext className='bg-white/75 hover:bg-orange-300 transition-colors duration-500' />
+			{!isMobile && (
+				<>
+					<CarouselPrevious className='bg-white/75 hover:bg-orange-300 transition-colors duration-500' />
+					<CarouselNext className='bg-white/75 hover:bg-orange-300 transition-colors duration-500' />
+				</>
+			)}
 		</CarouselComponent>
 	);
 }
